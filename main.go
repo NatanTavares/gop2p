eef0d04a6604a73996d3a0b69dc8bb391c31863a
NatanTavares <nattantavares.s15@gmail.com>
2024-07-11 14:20:01 -0300

    feat(p2p): Implements the basic logic for libp2p networking with stream handling

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
+	tls "github.com/libp2p/go-libp2p/p2p/security/tls"
+	"github.com/multiformats/go-multiaddr"
+)
+
+const protocolID = "/example/1.0.0"
+const discoveryNamespace = "example"
+
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	peerAddr := flag.String("peer-address", "", "peer address")
+	flag.Parse()
+
+	h, err := libp2p.New(
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+		libp2p.Security(tls.ID, tls.New),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer h.Close()
+
+	logger.Println("Addresses:", h.Addrs())
+	logger.Println("ID:", h.ID())
+
+	h.SetStreamHandler(protocolID, func(s network.Stream) {
+		logger.Println("Got a new stream!")
+		go writeCounter(s)
+		go readCounter(s)
+	})
+
+	discoveryService := mdns.NewMdnsService(h, discoveryNamespace, &discoveryNotifee{h: h})
+	defer discoveryService.Close()
+	if err := discoveryService.Start(); err != nil {
+		panic(err)
+	}
+
+	if *peerAddr != "" {
+		peerMA, err := multiaddr.NewMultiaddr(*peerAddr)
+		if err != nil {
+			panic(err)
+		}
+		peerAddrInfo, err := peer.AddrInfoFromP2pAddr(peerMA)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := h.Connect(context.Background(), *peerAddrInfo); err != nil {
+			panic(err)
+		}
+		logger.Println("Connected to", peerAddrInfo.String())
+
+		s, err := h.NewStream(context.Background(), peerAddrInfo.ID, protocolID)
+		if err != nil {
+			panic(err)
+		}
+
+		go writeCounter(s)
+		go readCounter(s)
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGKILL, syscall.SIGINT)
+	<-sigCh
+}
+
+func writeCounter(s network.Stream) {
+	var counter uint64
+	for {
+		<-time.After(time.Second)
+		counter++
+		err := binary.Write(s, binary.BigEndian, counter)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func readCounter(s network.Stream) {
+	for {
+		var counter uint64
+		err := binary.Read(s, binary.BigEndian, &counter)
+		if err != nil {
+			panic(err)
+		}
+		logger.Printf("Received %d from %s\n", counter, s.ID())
+	}
+}
+
+type discoveryNotifee struct {
+	h host.Host
+}
+
+func (n *discoveryNotifee) HandlePeerFound(peerInfo peer.AddrInfo) {
+	logger.Println("found peer", peerInfo.String())
+	if err := n.h.Connect(context.Background(), peerInfo); err != nil {
+		logger.Println("Failed to connect", err)
+	}
+}
